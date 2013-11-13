4ce9b38b6842b4ba7063f9bec01a3c19e1a86548
Rich Hildebrand <richhildebrand@gmail.com>
2013-11-12 19:44:13 -0500

    Red - 30 should comes after 15

---
+++ TennisKata/TennisGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace TennisKata
+{
+    public class TennisGame
+    {
+        public int PlayerOneScore;
+
+        public void PlayerOneScores() {
+            PlayerOneScore = 15;
+        }
+    }
+}
---
+++ TestTenniskata/Acceptance/GivenPlayerOneScores.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using TennisKata;
+
+namespace TestTenniskata.Acceptance
+{
+    [TestFixture]
+    public class GivenPlayerOneScores
+    {
+        [Test]
+        public void WhenHeHasZeroPoints_ThenHeShouldGetFifteenPoints() {
+            var tennisGame = new TennisGame();
+            tennisGame.PlayerOneScore = 0;
+
+            tennisGame.PlayerOneScores();
+
+            Assert.AreEqual(15, tennisGame.PlayerOneScore);
+        }
+
+        [Test]
+        public void WhenHeHasFifteenPoints_ThenHeShouldGetThirtyPoints()
+        {
+            var tennisGame = new TennisGame();
+            tennisGame.PlayerOneScore = 15;
+
+            tennisGame.PlayerOneScores();
+
+            Assert.AreEqual(30, tennisGame.PlayerOneScore);
+        }
+    }
+}
